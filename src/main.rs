8dc5fc456ac1854aed5c40ac7b9795757ce3b0c6
tommaho <tom@tommaho.com>
2024-01-15 16:41:37 -0500

    start clap cli config

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use clap::Parser;
+
+#[derive(Debug, Parser)]
+#[command(author, version, about)]
+
+
+struct Args {
+    /// Input text
+    #[arg(required(true))]
+    zip: Vec<String>,
+
+    /// More verbose output?
+    #[arg(short('n'))]
+    verbose: bool,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+    print!("{}", args.zip.join(" "));
 }
