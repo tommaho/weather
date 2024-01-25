b36a70d467bce343706a6a6e6c9d322881dc3f1c
tommaho <tom@tommaho.com>
2024-01-25 13:00:06 -0500

    removed clap dependency

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,4 @@
-use clap::Parser;
-
-#[derive(Debug, Parser)]
-#[command(author, version, about)]
-
-
-struct Args {
-    /// Input text
-    #[arg(required(true))]
-    zip: Vec<String>,
-
-    /// More verbose output?
-    #[arg(short('n'))]
-    verbose: bool,
-}
 
 fn main() {
-    let args = Args::parse();
-    print!("{}", args.zip.join(" "));
+    println!("Hello.");
 }
