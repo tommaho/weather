6aac82ca06be96fe17a327c90a3287fb35985b53
tommaho <tom@tommaho.com>
2024-04-29 09:11:33 -0400

    https

--- src/main.rs
+++ src/main.rs
@@ -153,7 +153,11 @@ fn parse_args() -> String{
 
     let args: Vec<String> = env::args().collect();
     if args.len() == 2 {
-        args[1].clone()
+        let zip = args[1].clone();
+
+        zip
+        // Validate zip code arg here
+
     } else {
         println!("\n**Unexpected or missing input. Defaulting to zip code 17701.**");
         "17701".to_string()
@@ -168,7 +172,7 @@ fn fetch_coords(api_key: &str, zip_code: &str) -> Result<Coords, reqwest::Error>
     //println!("The zip we'll look for is {} using {}", zip_code, api_key);
 
     let url = format!(
-        "http://api.openweathermap.org/geo/1.0/zip?zip={},US&appid={}",
+        "https://api.openweathermap.org/geo/1.0/zip?zip={},US&appid={}",
         zip_code, api_key
     );
 
@@ -184,7 +188,7 @@ fn fetch_weather(api_key: &str, coords: &Coords)-> Result<CurrentWeatherData, re
     //println!("We'll look up {} {} using {}", coords.lat, coords.lon, api_key);
     
     let url = format!(
-        "http://api.openweathermap.org/data/2.5/weather?lat={}&lon={}&appid={}&units=imperial",
+        "https://api.openweathermap.org/data/2.5/weather?lat={}&lon={}&appid={}&units=imperial",
         coords.lat, coords.lon, api_key
     );
 
@@ -193,13 +197,12 @@ fn fetch_weather(api_key: &str, coords: &Coords)-> Result<CurrentWeatherData, re
     Ok(response)
 }
 
-//get forecast for a lat lon, using openweathermap api
 fn fetch_forecast(api_key: &str, coords: &Coords)-> Result<WeatherForecast, reqwest::Error>{
     //debug
     //println!("We'll look up {} {} using {}", coords.lat, coords.lon, api_key);
     
     let url = format!(
-        "http://api.openweathermap.org/data/2.5/forecast?lat={}&lon={}&appid={}&units=imperial",
+        "https://api.openweathermap.org/data/2.5/forecast?lat={}&lon={}&appid={}&units=imperial",
         coords.lat, coords.lon, api_key
     );
 
@@ -213,8 +216,10 @@ fn get_api_key() -> String {
 
     let config_content = std::fs::read_to_string("Config.toml")
         .expect("Unable to read config.");
+
     let config: Value = toml::from_str(&config_content)
         .expect("Error parsing config.");
+    
     let api_key = config["weather_api_key"].as_str()
         .expect("API_KEY not found in config file");
 
